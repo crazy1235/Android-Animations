9e9e4f24851058dd5f6ec6c13316108e0f8aea5b
jacksen <951532416@qq.com>
2016-04-25 23:31:53 +0800

    update

--- TransitionDemo/app/src/main/java/com/jacksen/transitiondemo/MainActivity.java
+++ TransitionDemo/app/src/main/java/com/jacksen/transitiondemo/MainActivity.java
@@ -1,13 +1,52 @@
 package com.jacksen.transitiondemo;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Scene;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Scene aScene;
+    private Scene bScene;
+
+    private FrameLayout rootScene;
+
+    private Button nextSceneBtn;
+
+    private boolean flag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rootScene = (FrameLayout) findViewById(R.id.scene_root);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            aScene = Scene.getSceneForLayout(rootScene, R.layout.a_scene, this);
+            bScene = Scene.getSceneForLayout(rootScene, R.layout.b_scene, this);
+        }
+
+        nextSceneBtn = (Button) findViewById(R.id.next_scene_btn);
+
+        nextSceneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    if (flag) {
+                        TransitionManager.go(aScene);
+                        flag = false;
+                    } else {
+                        TransitionManager.go(bScene);
+                        flag = true;
+                    }
+                }
+            }
+        });
     }
 }
